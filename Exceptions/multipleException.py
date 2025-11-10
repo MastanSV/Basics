9728bd41e1a87efd3aaf7c92b1b47445fb84efd6
Vali S, Mastan <mastanvali157@gmail.com>
2025-11-10 23:28:24 +0530

    added scenario try without except & only finally

--- Exceptions/multipleException.py
+++ Exceptions/multipleException.py
@@ -7,3 +7,10 @@ else: # else block will run only when after successful execution of try block
     print('Successful excution of try block') 
 finally:
     print('will run always')
+
+
+#scenario without except block
+try:
+    x = 10/2
+finally:
+    print('valid')
